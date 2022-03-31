fe69aec0a4ad4f4926d4f0ad57afecbcc3c552cb
Davide Venturelli <dventurelli90@gmail.com>
2022-03-31 16:48:31 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 //debug 
 console.log('JS OK!');
 
-//MILESTONE 1
-//Replica della grafica con la possibilità di avere messaggi scritti
-//dall’utente(verdi) e dall’interlocutore(bianco) assegnando due classi CSS diverse
-//Visualizzazione dinamica della lista contatti: tramite la direttiva v -for,
-//visualizzare nome e immagine di ogni contatto
+/*
+MILESTONE 1
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente(verdi) e
+  dall’interlocutore(bianco) assegnando due classi CSS diverse
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v -for, visualizzare
+  nome e immagine di ogni contatto [X]
+
+MILESTONE 2
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v -for, visualizzare tutti i
+  messaggi relativi al contatto attivo all’interno del pannello della conversazione
+● Click sul contatto mostra la conversazione del contatto cliccato [X]
+
+MILESTONE 3
+● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+  “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+  un “ok” come risposta, che apparirà dopo 1 secondo.
+
+MILESTONE 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+  contatti il cui nome contiene le lettere inserite(es, Marco, Matteo Martina -> Scrivo
+  “mar” rimangono solo Marco e Martina
+*/
 
 const contacts = [
     {
@@ -175,7 +193,7 @@ const app = new Vue({
     el: '#boolzapp',
     data: {
         contacts,
-        selectedContact: null,
+        selectedContact: contacts[0]
     },
     methods: {
         imgURLAvatar(imgContact) {
@@ -190,5 +208,8 @@ const app = new Vue({
             //console.log(lastMessage);
             return lastMessage;
         },
+        selectContact(contact) {
+            this.selectedContact = contact;
+        }
     }
 })
